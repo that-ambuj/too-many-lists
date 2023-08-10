e46254ea6fc10cb59dbdf9ea73718d94a16490d0
Ambuj Singh <ambujs89@gmail.com>
2023-08-11 01:15:59 +0530

    implement an unsafe queue (linked list)

---
+++ src/fifth.rs
@@ -0,0 +1,230 @@
+use std::ptr;
+
+pub struct List<T> {
+    head: Link<T>,
+    tail: *mut Node<T>,
+}
+
+type Link<T> = *mut Node<T>;
+
+struct Node<T> {
+    elem: T,
+    next: Link<T>,
+}
+
+impl<T> List<T> {
+    #[must_use]
+    pub fn new() -> Self {
+        List {
+            head: ptr::null_mut(),
+            tail: ptr::null_mut(),
+        }
+    }
+
+    pub fn push(&mut self, elem: T) {
+        let new_tail = Box::into_raw(Box::new(Node {
+            elem,
+            next: ptr::null_mut(),
+        }));
+
+        if self.tail.is_null() {
+            // If the old tail did not exist, update the head to point to it.
+            self.head = new_tail;
+        } else {
+            // If the tail existed, update it to point to the new tail
+            unsafe {
+                (*self.tail).next = new_tail;
+            }
+        }
+
+        self.tail = new_tail;
+    }
+
+    pub fn pop(&mut self) -> Option<T> {
+        if self.head.is_null() {
+            None
+        } else {
+            unsafe {
+                let head = Box::from_raw(self.head);
+
+                // Move the head forward into the list (i.e. `next`)
+                self.head = head.next;
+
+                // If the the head has reached the end of the list
+                if self.head.is_null() {
+                    // set the tail to `null` too
+                    self.tail = ptr::null_mut();
+                }
+
+                Some(head.elem)
+            }
+        }
+    }
+
+    #[allow(clippy::must_use_candidate)]
+    pub fn iter(&self) -> Iter<T> {
+        unsafe {
+            Iter {
+                next: self.head.as_ref(),
+            }
+        }
+    }
+
+    pub fn iter_mut(&mut self) -> IterMut<T> {
+        unsafe {
+            IterMut {
+                next: self.head.as_mut(),
+            }
+        }
+    }
+
+    #[allow(clippy::must_use_candidate)]
+    pub fn peek(&self) -> Option<&T> {
+        unsafe { self.head.as_ref().map(|node| &node.elem) }
+    }
+
+    pub fn peek_mut(&mut self) -> Option<&mut T> {
+        unsafe { self.head.as_mut().map(|node| &mut node.elem) }
+    }
+}
+
+pub struct IntoIter<T>(List<T>);
+
+pub struct Iter<'a, T> {
+    next: Option<&'a Node<T>>,
+}
+
+pub struct IterMut<'a, T> {
+    next: Option<&'a mut Node<T>>,
+}
+
+impl<T> IntoIterator for List<T> {
+    type Item = T;
+    type IntoIter = IntoIter<T>;
+
+    fn into_iter(self) -> Self::IntoIter {
+        IntoIter(self)
+    }
+}
+
+impl<T> Iterator for IntoIter<T> {
+    type Item = T;
+
+    fn next(&mut self) -> Option<Self::Item> {
+        self.0.pop()
+    }
+}
+
+impl<'a, T> Iterator for Iter<'a, T> {
+    type Item = &'a T;
+
+    fn next(&mut self) -> Option<Self::Item> {
+        self.next.map(|node| {
+            unsafe {
+                self.next = node.next.as_ref();
+            }
+            &node.elem
+        })
+    }
+}
+
+impl<'a, T> Iterator for IterMut<'a, T> {
+    type Item = &'a mut T;
+
+    fn next(&mut self) -> Option<Self::Item> {
+        self.next.take().map(|node| {
+            unsafe {
+                self.next = node.next.as_mut();
+            }
+            &mut node.elem
+        })
+    }
+}
+
+impl<T> Default for List<T> {
+    fn default() -> Self {
+        Self::new()
+    }
+}
+
+impl<T> Drop for List<T> {
+    fn drop(&mut self) {
+        while self.pop().is_some() {}
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn basics() {
+        let mut list = List::new();
+
+        assert_eq!(list.pop(), None);
+
+        list.push(1);
+        list.push(2);
+        list.push(3);
+
+        assert_eq!(list.pop(), Some(1));
+        assert_eq!(list.pop(), Some(2));
+
+        list.push(4);
+        list.push(5);
+
+        assert_eq!(list.pop(), Some(3));
+        assert_eq!(list.pop(), Some(4));
+
+        // check exhaustion
+        assert_eq!(list.pop(), Some(5));
+        assert_eq!(list.pop(), None);
+
+        // Check the exhaustion case fixed the pointer right
+        list.push(6);
+        list.push(7);
+
+        assert_eq!(list.pop(), Some(6));
+        assert_eq!(list.pop(), Some(7));
+        assert_eq!(list.pop(), None);
+    }
+
+    #[test]
+    fn miri_food() {
+        let mut list = List::new();
+
+        list.push(1);
+        list.push(2);
+        list.push(3);
+
+        assert_eq!(list.pop(), Some(1));
+        list.push(4);
+        assert_eq!(list.pop(), Some(2));
+        list.push(5);
+
+        assert_eq!(list.peek(), Some(&3));
+        assert_eq!(list.peek(), Some(&3));
+
+        list.push(6);
+        list.peek_mut().map(|x| *x *= 10);
+
+        assert_eq!(list.pop(), Some(30));
+
+        for elem in list.iter_mut() {
+            *elem *= 100;
+        }
+
+        let mut iter = list.iter();
+
+        assert_eq!(iter.next(), Some(&400));
+        assert_eq!(iter.next(), Some(&500));
+        assert_eq!(iter.next(), Some(&600));
+        assert_eq!(iter.next(), None);
+        assert_eq!(iter.next(), None);
+
+        assert_eq!(list.pop(), Some(400));
+        list.peek_mut().map(|x| *x *= 10);
+        assert!(list.peek() == Some(&5000));
+        list.push(7);
+    }
+}
